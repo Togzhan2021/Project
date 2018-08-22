3564efc63591a9f8b07d46701753233ac44ec5ef
Toki <togzhan.kurmanbek@mail.ru>
2018-08-22 17:12:45 +0600

    Horizontal Line (Vertical)

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -11,37 +11,17 @@ namespace Project
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            //p1.Draw();
+           // p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            //p2.Draw();
+           // p2.Draw();
 
-            List<int> numList = new List<int>();
+            HorizontalLine hline = new HorizontalLine(5, 10, 8, '+');
+            hline.Draw();
 
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            VerticalLine vline = new VerticalLine(5, 5, 10, '+');
+            //vline.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-
-            pList.Add(p1);
-            pList.Add(p2);
-
-            foreach (Point i in pList)
-            {
-                i.Draw();
-            }
 
             Console.ReadLine();
         }
